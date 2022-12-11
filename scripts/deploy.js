ef891066d1adeee31e20398f039d3b9b5145d9b8
Gerardo Ornelas <gerardo@ornelastechnologies.com>
2022-12-11 08:53:33 -0600

    update deploy and abi

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,18 +5,30 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const { ethers } = require("ethers");
 
 async function main() {
-  const NAME = 'Dapp University'
-  const SYMBOL = 'DAPP'
-  const MAX_SUPPLY = '1000000'
+  const NAME = "Wun Punks";
+  const SYMBOL = "WP";
+  const COST = ethers.utils.parseUnits("10", "ether");
+  const MAX_SUPPLY = 25;
+  const NFT_MINT_DATE = (Date.now() + 60000).toString().slice(0, 10);
+  const IPFS_METADATA_URI =
+    "ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/";
 
   // Deploy Token
-  const Token = await hre.ethers.getContractFactory('Token')
-  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
+  const NFT = await hre.ethers.getContractFactory("NFT");
+  let nft = await NFT.deploy(
+    NAME,
+    SYMBOL,
+    COST,
+    MAX_SUPPLY,
+    NFT_MINT_DATE,
+    IPFS_METADATA_URI
+  );
 
-  await token.deployed()
-  console.log(`Token deployed to: ${token.address}\n`)
+  await nft.deployed();
+  console.log(`NFT deployed to: ${nft.address}\n`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
